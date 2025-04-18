111f5bf16ee15e94d73c7fad23df1bd752e18527
Faso-main <faso.312@yandex.ru>
2025-04-18 10:53:23 +0400

    setup pathing for medical data | 18_4_25 10:53

--- AutoML/MedDataLightML.py
+++ AutoML/MedDataLightML.py
@@ -2,10 +2,15 @@ import lightgbm as lgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
+import os
+
+
+medical_data_path=os.path.join('AutoML','medica_data.py')
+labels_data_path=os.path.join('AutoML','medica_data.py')
 
 # Подготовка данных
-X = np.array(medical_data)
-y = np.array(labels).flatten()
+X = np.array(medical_data_path)
+y = np.array(labels_data_path).flatten()
 
 # Разделение данных на обучающую и тестовую выборки (для оценки модели)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
